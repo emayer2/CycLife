33cb7a73efa0dbc294671e8b1ac9553459c3277a
emayer2 <bohemian.mayer@gmail.com>
2017-04-03 21:09:42 -0700

    Working on bluetooth, nexus 5x doesn't like it

--- App/app/src/main/java/edu/uw/cyclife/cyclifeapp/MainActivity.java
+++ App/app/src/main/java/edu/uw/cyclife/cyclifeapp/MainActivity.java
@@ -1,11 +1,15 @@
 package edu.uw.cyclife.cyclifeapp;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,14 +17,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Set;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final int REQUEST_ENABLE_BT = 1;  // For notify intent
+    private BluetoothAdapter bluetoothAdapter;
+    private ArrayAdapter<String> BTArrayAdapter;
+    private Set<BluetoothDevice> pairedDevices;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            ((TextView) findViewById(R.id.main_text))
+                    .setText("FOUND");
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found an object
+                BluetoothDevice foundDevice =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = foundDevice.getName();
+                String deviceHWAddr = foundDevice.getAddress();
+//                CharSequence currText = ((TextView) findViewById(R.id.main_text)).getText();
+                ((TextView) findViewById(R.id.main_text))
+                        .setText("Device: " + deviceName +
+                                  ", Addr: " + deviceHWAddr + "\n");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +75,19 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+//        // Enable bluetooth discovery
+//        Intent discoverableIntent =
+//                new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+//        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+//        startActivity(discoverableIntent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister the ACTION_FOUND receiver
+        unregisterReceiver(mReceiver);
     }
 
     @Override
@@ -91,6 +133,32 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, ScrollingActivity.class);
             startActivity(intent);
         } else if (id == R.id.nav_gallery) {
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (bluetoothAdapter == null) {
+                // Device does not support Bluetooth
+                ((TextView)findViewById(R.id.main_text)).setText("No Bluetooth");
+            } else {
+                // Bluetooth found, check if enabled and prompt
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+
+                // Start discovery
+                ((TextView) findViewById(R.id.main_text)).setText("Discovering...");
+                bluetoothAdapter.startDiscovery();
+                // Bluetooth discovery
+                IntentFilter filter = new IntentFilter();
+                filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+                filter.addAction(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(mReceiver, filter);
+//                ((TextView) findViewById(R.id.main_text)).setText("Done!");
+//                String out = "";
+//                for (BluetoothDevice d : bluetoothAdapter.getBondedDevices()) {
+//                    out += ("Device: " + d.getName() + ", Addr: " + d.getAddress() + "\n");
+//                }
+//                ((TextView) findViewById(R.id.main_text)) .setText(out);
+            }
 
         } else if (id == R.id.nav_share) {
 
